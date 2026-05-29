6fecb17540c74646d811e925d3c0615c0bd103d6
laszlobortel <bortel.laszlo@telekom.hu>
2026-05-29 20:50:07 +0200

    Update esxi.mjs
    threads=6,compression=fastlz

--- @xen-orchestra/vmware-explorer/esxi.mjs
+++ @xen-orchestra/vmware-explorer/esxi.mjs
@@ -549,7 +549,7 @@ export default class Esxi extends EventEmitter {
     }
   }
 
-  async spawnNbdKitProcess(vmId, diskPath, { singleLink = false, threads = 5, compression = 'fastlz' } = {}) {
+  async spawnNbdKitProcess(vmId, diskPath, { singleLink = false, threads = 6, compression = 'fastlz' } = {}) {
     const key = `${vmId}/${diskPath}/${singleLink}`
     if (!this.#nbdServers.has(key)) {
       const thumbprint = await this.#getServerThumbprint()
